5943ad1f6a3b4f080c3d8c5a654a26dbb44235a5
xeterguyt <75139409+xeterguyt@users.noreply.github.com>
2026-05-20 14:55:43 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,13 @@
-const CACHE = 'titledata-v2';
-
-self.addEventListener('install', e => {
-  self.skipWaiting();
-});
+const CACHE = 'utm-v1';
+self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
-  // Clear ALL old caches on activate
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.map(k => caches.delete(k)))
-  ));
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
   self.clients.claim();
 });
 self.addEventListener('fetch', e => {
-  // Network-first: always try GitHub first, fall back to cache only if offline
   e.respondWith(
     fetch(e.request)
-      .then(resp => {
-        // Clone and cache fresh response
-        const clone = resp.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return resp;
-      })
+      .then(resp => { const c=resp.clone(); caches.open(CACHE).then(ca=>ca.put(e.request,c)); return resp; })
       .catch(() => caches.match(e.request))
   );
 });
